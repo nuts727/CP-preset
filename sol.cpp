60d1a7b39df6636fbf89b475c1a051abc92397d0
nuts727 <khanhvinh221207@gmail.com>
2025-08-31 20:39:34 +0700

    first commit

---
+++ sol.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Type aliases
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+ 
+// Utility macros
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define read(x) for (int& i : x) cin >> i;
+#define write(stream, x) for (const auto& i : x) stream << i << ' '; stream << endl;
+#define dbg(x) cerr << "[DEBUG] Line " << __LINE__ << ": " << #x << " = " << (x) << endl;
+ 
+// Fast I/O
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define IO(file) freopen(file ".inp", "r", stdin); freopen(file ".out", "w", stdout);
+ 
+// Execution time measurement
+#define START_TIMER auto start_time = chrono::high_resolution_clock::now();
+#define END_TIMER auto end_time = chrono::high_resolution_clock::now(); \
+        cerr << "Execution Time: " << chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count() << " ms" << endl;
+ 
+// Constants
+
+int main() {
+    FASTIO
+    IO("test")
+    
+    return 0;
+}
